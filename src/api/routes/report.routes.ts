b38e52efa305af119b16b2e011b69232f92ff885
Abhishek <bhattabhi013@gmail.com>
2025-04-24 23:14:58 +0530

    completed first commit

--- src/api/routes/report.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getReport } from '../controllers/report.controller';
-
-const router = Router();
-
-router.get('/report/:reportId', getReport);
-
-export default router;
